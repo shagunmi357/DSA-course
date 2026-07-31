874c58a6a1f3e22203ce21bf45ac4a221fdc2687
Shagun <shagunmi357@gmail.com>
2026-07-31 20:25:58 +0530

    Patterns

---
+++ pattern.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i = 0; i <= 4; i++){
+        for(int j = 1; j <= 4-i+1; j++){
+        cout << "* " << " ";
+    }
+   cout << endl;
+}
+    return 0;
+}

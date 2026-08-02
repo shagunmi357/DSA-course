9c4ed981ce5d5475796f1bf9d3d307b78dca1e0c
Shagun <shagunmi357@gmail.com>
2026-08-02 14:14:33 +0530

    Array Questions

---
+++ Array.cpp/largest_element.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int largestElement(vector<int>&arr ) {
+      int largest = arr[0];
+      for(int i = 0 ; i < arr.size() ; i++){
+        if(arr[i]> largest){
+            largest = arr[i];
+        }
+        
+      }
+      return largest;
+    }
+    };
+    int main() {
+        vector<int> arr = {1, 2, 3, 4, 5};
+        Solution sol;
+        int largest = sol.largestElement(arr);
+        cout << "The largest element is: " << largest ;
+        return 0;
+    }

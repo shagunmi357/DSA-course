ad5a7ced8f454109c62b50e9db61e4ef9fe2d980
Shagun <shagunmi357@gmail.com>
2026-08-04 19:46:58 +0530

    second largest

--- Array.cpp/tempCodeRunnerFile.cpp
+++ Array.cpp/tempCodeRunnerFile.cpp
@@ -1,4 +1,4 @@
-// // Better Approach
+// Better Approach
 // #include<bits/stdc++.h>
 // using namespace std;
 // class Solution{
@@ -27,31 +27,3 @@
 //     cout << "The second largest element is: " << secondlargest ;
 //     return 0;
 // }
-// optimal Approach
-#include <bits/stdc++.h>
-using namespace std;
-class Solution{
-    public:
-    int secondlargest(vector<int> &arr){
-        int largest = arr[0];
-        int secondlargest = -1;
-        for(int i = 0; i<arr.size() ; i++){
-            if (arr[i] > largest){
-                secondlargest = largest;
-                largest = arr[i];
-            }
-            else if(arr[i] <largest && arr[i]> secondlargest ){
-               secondlargest = arr[i];
-            }
-            
-        }
-        return secondlargest;
-    }
-};
-int main(){  
-    vector<int> arr = {1, 2, 3,3,2,6,4,5,5,1};
-    Solution sol;
-    int secondlargest = sol.secondlargest(arr);
-    cout << "The second largest element is: " << secondlargest ;
-    return 0;
-}
